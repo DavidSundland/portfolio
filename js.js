971fbb97854ebb821bea9e932e23fcffcdb2b5d5
David Sundland <info@districtconcerts.com>
2018-06-24 21:59:31 -0400

    profile pic options

--- js.js
+++ js.js
@@ -1,7 +1,7 @@
 function hoverHighlight() {
     $(".projectPic").hover(
         function() {$(this).css("opacity",1); $(this).css("border", "none")},
-        function() {$(this).css("opacity",.9); $(this).css("border", "4px outset #FFC49E")}
+        function() {$(this).css("opacity",.9); $(this).css("border", "2px solid #555")}
     );
 }
 
